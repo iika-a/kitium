335aa3e09798ec5e4fd68197de05f26fdfdb2e77
donutslaptop <tony.anastasiadis10@gmail.com>
2025-02-03 14:07:40 -0500

    Add Kitium Golem entity to ModEntities
    - Register Kitium Golem entity type
    - Set spawn group to CREATURE
    - Use dimensions of Zombie entity
    - Update `registerModEntities` method
    - Ensure unique identifier for Kitium Golem entity

--- src/main/java/net/tonyearlnate/unbounded_unknown/entity/ModEntities.java
+++ src/main/java/net/tonyearlnate/unbounded_unknown/entity/ModEntities.java
@@ -15,6 +15,12 @@ public class ModEntities {
             Identifier.of(UnboundedUnknown.MOD_ID, "lightning_bottle"),
             EntityType.Builder.create(LightningBottleEntity::create, SpawnGroup.MISC).build(RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(UnboundedUnknown.MOD_ID, "lightning_bottle")))
     );
+    
+	public static final EntityType<KitiumGolemEntity> KITIUM_GOLEM_ENTITY_TYPE = Registry.register(
+			Registries.ENTITY_TYPE, Identifier.of(UnboundedUnknown.MOD_ID, "kitium_golem"),
+			EntityType.Builder.create(KitiumGolemEntity::new, SpawnGroup.CREATURE)
+					.dimensions(EntityType.ZOMBIE.getDimensions()).build(RegistryKey.of(RegistryKeys.ENTITY_TYPE,
+							Identifier.of(UnboundedUnknown.MOD_ID, "kitium_golem"))));
 
     public static void registerModEntities() {
         UnboundedUnknown.LOGGER.info("Registering Entities for " + UnboundedUnknown.MOD_ID);
