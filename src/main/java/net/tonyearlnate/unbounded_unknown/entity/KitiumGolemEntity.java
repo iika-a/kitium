3a9342fbb20d2190d159d78417d13b496ac26111
donutslaptop <tony.anastasiadis10@gmail.com>
2025-02-11 10:49:40 -0500

    git golem push

---
+++ src/main/java/net/tonyearlnate/unbounded_unknown/entity/KitiumGolemEntity.java
@@ -0,0 +1,12 @@
+package net.tonyearlnate.unbounded_unknown.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.world.World;
+
+public class KitiumGolemEntity extends PathAwareEntity {
+	 
+    public KitiumGolemEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+        super(entityType, world);
+    }
+}
