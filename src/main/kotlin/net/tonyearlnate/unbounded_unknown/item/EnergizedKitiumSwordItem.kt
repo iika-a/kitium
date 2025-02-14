da33e87ef9aefc2d96cfef5f452dabc0f6b7b47a
iika-a <epatrick7375@gmail.com>
2025-02-14 13:52:10 -0500

    made energized kitium sword give blindness instead

--- src/main/kotlin/net/tonyearlnate/unbounded_unknown/item/EnergizedKitiumSwordItem.kt
+++ src/main/kotlin/net/tonyearlnate/unbounded_unknown/item/EnergizedKitiumSwordItem.kt
@@ -8,7 +8,7 @@ import net.minecraft.item.SwordItem
 
 class EnergizedKitiumSwordItem(settings: Settings) : SwordItem(ModToolMaterials.ENERGIZED_KITIUM, 3.0f, -2.4f, settings) {
     override fun postHit(stack: ItemStack?, target: LivingEntity?, attacker: LivingEntity?): Boolean {
-        target?.addStatusEffect(StatusEffectInstance(StatusEffects.SLOWNESS, 40, 1))
+        target?.addStatusEffect(StatusEffectInstance(StatusEffects.BLINDNESS, 40, 1))
         return super.postHit(stack, target, attacker)
     }
 }
